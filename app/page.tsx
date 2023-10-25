8af8293edb1b89a9016c11e5b4b0a8c0b62a7173
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-10-25 17:57:16 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { Hero } from './components';
 
 export default function Home() {
   return (
     <main className="overflor-hidden">
+      <Hero/>
     </main>
   )
 }
