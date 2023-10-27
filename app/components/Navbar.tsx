a5d27166f092d7d0e835d28a6164c6fe22190011
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-10-27 10:41:03 -0300

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,9 @@ const Navbar = () => {
         <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
             <Link href="/" className="flex justify-center items-center"><Image src='/logo.svg' alt='Car Shop Logo' width={118} height={18}
             className="object-contain"/></Link>
+            <Link href="/construction">
         <CustomButton title="Sign In" btnType="button" containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"/>
+            </Link>
         </nav>
     </header>
   )
