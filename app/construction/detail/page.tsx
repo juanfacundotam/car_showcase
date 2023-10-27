77468fb2807355ae3bd2c072839d63923a3fbee6
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-10-27 10:25:08 -0300

    Create page.tsx

---
+++ app/construction/detail/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Detail = () => {
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x pb-36">
+        <h1 className="hero__title">Still, there is nothing.</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
