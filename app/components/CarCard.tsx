8716745039665442eb998449ebd33b2480e32c9f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-03 16:42:57 -0300

    Update CarCard.tsx

--- app/components/CarCard.tsx
+++ app/components/CarCard.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "../types";
 import { calculateCarRent } from "../utils";
-import  CustomButton  from "./CustomButton";
+import CustomButton from "./CustomButton";
 import { CarDetails } from ".";
 
 interface CarCardProps {
@@ -13,7 +13,7 @@ interface CarCardProps {
 const CarCard = ({ car }: CarCardProps) => {
   const { city_mpg, year, make, model, transmission, drive } = car;
 
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   const carRent = calculateCarRent(city_mpg, year);
 
@@ -52,40 +52,29 @@ const CarCard = ({ car }: CarCardProps) => {
             </p>
           </div>
           <div className="flex flex-col justify-center items-center gap-2">
-            <Image
-              src="/tire.svg"
-              width={20}
-              height={20}
-              alt="tire"
-            />
-            <p className="text-[14px]">
-              {drive.toUpperCase()}
-            </p>
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
           </div>
           <div className="flex flex-col justify-center items-center gap-2">
-            <Image
-              src="/gas.svg"
-              width={20}
-              height={20}
-              alt="gas"
-            />
-            <p className="text-[14px]">
-              {city_mpg} MPG
-            </p>
+            <Image src="/gas.svg" width={20} height={20} alt="gas" />
+            <p className="text-[14px]">{city_mpg} MPG</p>
           </div>
         </div>
 
         <div className="car-card__btn-container">
-            <CustomButton title="View More" containerStyles="w-full py-[16px] rounded-full bg-primary-blue" textStyles="text-white text-[14px] leading-[17px] font-bold"
+          <CustomButton
+            title="View More"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
             rightIcon="/right-arrow.svg"
-            handleClick={() => {setIsOpen(true)
-
-            }}/>
+            handleClick={() => {
+              setIsOpen(true);
+            }}
+          />
         </div>
-
       </div>
 
-      <CarDetails/>
+      <CarDetails />
     </div>
   );
 };
