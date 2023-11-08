6222aad15e2e0fd021aaaa57e90687fc6a4ffdf3
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-08 09:12:49 -0300

    Update CarDetails.tsx

--- app/components/CarDetails.tsx
+++ app/components/CarDetails.tsx
@@ -1,29 +1,39 @@
-import Image from 'next/image';
-import { Fragment } from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import {CarProps} from '../types';
+import Image from "next/image";
+import { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { CarProps } from "../types";
 
 interface CarDetailsProps {
   isOpen: boolean;
   closeModal: () => void;
-  car: CarProps
+  car: CarProps;
 }
 
-const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
+const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
   return (
     <>
-    <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className='relative z-10' onClose={closeModal}>
-      <Transition.Child>
-        <div className='fixed inset-0 bg-black bg-opacity-25'/>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="easy-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="easy-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
 
-      </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center"></div>
 
-      </Dialog>
-
-    </Transition>
+          </div>
+        </Dialog>
+      </Transition>
     </>
-  )
-}
+  );
+};
 
-export default CarDetails
+export default CarDetails;
