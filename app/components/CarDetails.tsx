75b3c3761c2df9a9abbe63066ee413b0142ecef1
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-08 09:08:47 -0300

    Update CarDetails.tsx

--- app/components/CarDetails.tsx
+++ app/components/CarDetails.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Image from 'next/image';
+import { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import {CarProps} from '../types';
 
-const CarDetails = () => {
+interface CarDetailsProps {
+  isOpen: boolean;
+  closeModal: () => void;
+  car: CarProps
+}
+
+const CarDetails = ({isOpen, closeModal, car}: CarDetailsProps) => {
   return (
-    <div>CarDetails</div>
+    <>
+    <Transition appear show={isOpen} as={Fragment}>
+      <Dialog as="div" className='relative z-10' onClose={closeModal}>
+      <Transition.Child>
+        <div className='fixed inset-0 bg-black bg-opacity-25'/>
+
+      </Transition.Child>
+
+      </Dialog>
+
+    </Transition>
+    </>
   )
 }
 
