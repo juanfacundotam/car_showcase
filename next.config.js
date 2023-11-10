8ac7f3061be396fccefa7ae93dd1ce313026c3de
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 11:56:38 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    typescript: {
-        ignoreBuildErrors: true,
+    images: {
+        domains: ["cdn.imagin.studio"]
     }
 }
 
