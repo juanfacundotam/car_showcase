ed7de73825c59854382b7920612074c6a41bac25
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 22:48:14 -0300

    Update SearchManufacturer.tsx

--- app/components/SearchManufacturer.tsx
+++ app/components/SearchManufacturer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, Fragment } from "react";
-import { SearchManufacturerProps } from "../types";
+import { SearchManufacturerProps } from "../types/index";
 import { Combobox, Transition } from "@headlessui/react";
 import Image from "next/image";
 import { manufacturers } from "../constants";
