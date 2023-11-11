ebbcf26df7002590b61733f83bae48d4750d36a9
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-11 08:29:27 -0300

    Update SearchBar.tsx

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -63,7 +63,7 @@ const SearchBar = () => {
       <div className='searchbar__item'>
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManuFacturer={setManufacturer}
+          setManufacturer={setManufacturer}
         />
         <SearchButton otherClasses='sm:hidden' />
       </div>
