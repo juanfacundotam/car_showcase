8945f5cf1fe48bd8548e598ad5aa4aad77994c19
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 22:54:55 -0300

    Update SearchBar.tsx

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -19,7 +19,7 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-  const [manufacturer, setManuFacturer] = useState("");
+  const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
 
   const router = useRouter();
@@ -63,7 +63,7 @@ const SearchBar = () => {
       <div className='searchbar__item'>
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManuFacturer={setManuFacturer}
+          setManufacturer={setManufacturer}
         />
         <SearchButton otherClasses='sm:hidden' />
       </div>
