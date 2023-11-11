201c969468168369a81eccd48fab31ca9a10995f
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 23:00:31 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { fetchCars } from "./utils/index";
-import { HomeProps } from "./types/index";
-import { fuels, yearsOfProduction } from "./constants/index";
+import { fetchCars } from "./utils";
+import { HomeProps } from "./types";
+import { fuels, yearsOfProduction } from "./constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "./components";
 
 export default async function Home({ searchParams }: HomeProps) {
