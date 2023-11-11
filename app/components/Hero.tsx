31f5842dbcd9ec098c7114a0eb089259b9c40ef4
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 22:55:03 -0300

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,32 +1,44 @@
 "use client"
-import React from 'react';
-import Image from 'next/image';
+
+import Image from "next/image";
 import CustomButton from './CustomButton';
 
 const Hero = () => {
-const handleScroll = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-}
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
-    <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'>
-<h1 className='hero__title'>Find, book, or rent a car -- quickly and easily!</h1>
-<p className='hero__subtitle'> Streamline yout car rental experience whit our efforless boking process.</p>
-       
-       <CustomButton title="Explore Cars"
-       containerStyles="bg-primary-blue text-white rounded-full mt-10" 
-       handleClick={handleScroll}
-       />
-        </div>
-        <div className='hero__image-container'>
-            <div className='hero__image'>
-                <Image src="/hero.png" alt='hero' fill className='object-contain'/>
-            </div>
-                <div className='hero__image-overlay'></div>
-        </div>
-        </div>
-  )
-}
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Find, book, rent a car—quick and super easy!
+        </h1>
 
-export default Hero
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
