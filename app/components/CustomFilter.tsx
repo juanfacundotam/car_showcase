829d54b4470cdf0f4086d7ac80467d08039175ea
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 22:18:31 -0300

    Update CustomFilter.tsx

--- app/components/CustomFilter.tsx
+++ app/components/CustomFilter.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+"use client";
+
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Listbox, Transition } from "@headlessui/react";
+
+import { CustomFilterProps } from "@types";
+import { updateSearchParams } from "@utils";
 
 const CustomFilter = () => {
   return (
