e88238ccb325769b9b032df8958a24a3bd2e4c27
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 22:55:07 -0300

    Update CustomButton.tsx

--- app/components/CustomButton.tsx
+++ app/components/CustomButton.tsx
@@ -1,24 +1,28 @@
-"use client"
-import Image from "next/image"
-import { CustomButtonProps } from "../types"
+"use client";
 
-const CustomButton = ({title, containerStyles, handleClick, btnType, textStyles, rightIcon}: CustomButtonProps) => {
-  return (
-    <button
-    disabled={false}
+import Image from "next/image";
+
+import { CustomButtonProps } from "../types/index";
+
+const Button = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={isDisabled}
     type={btnType || "button"}
     className={`custom-btn ${containerStyles}`}
-    onClick={handleClick}>
-    
-      <span className={`flex-1 ${textStyles}`}>{title}</span>
-      {rightIcon && (
-        <div className="relative w-6 h-6">
-          <Image src={rightIcon}
-          alt="right icon" fill className="object-contain"/>
-        </div>
-      )}
-    </button>
-  )
-}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+);
 
-export default CustomButton
+export default Button;
