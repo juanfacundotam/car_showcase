b19a555f6ae1fada7fbcb7e68f638a9b3fe56857
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-11-10 22:18:46 -0300

    Update CustomFilter.tsx

--- app/components/CustomFilter.tsx
+++ app/components/CustomFilter.tsx
@@ -9,6 +9,16 @@ import { CustomFilterProps } from "@types";
 import { updateSearchParams } from "@utils";
 
 const CustomFilter = () => {
+  const router = useRouter();
+  const [selected, setSelected] = useState(options[0]); // State for storing the selected option
+
+  // update the URL search parameters and navigate to the new URL
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase());
+
+    router.push(newPathName);
+  };
+
   return (
     <div>CustomFilter</div>
   )
